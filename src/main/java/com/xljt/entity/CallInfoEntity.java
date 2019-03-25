db795d0c419afaec35697b00cb2c075129947a8e
iamduckegg <1666789590@qq.com>
2019-03-25 10:26:06 +0800

    修改车辆信息实体类类名

---
+++ src/main/java/com/xljt/entity/CallInfoEntity.java
@@ -0,0 +1,74 @@
+package com.xljt.entity;
+
+/**
+ * 车辆信息
+ * @author ZX
+ */
+public class CallInfoEntity {
+	
+	private int carId;            //车辆id
+	private String carNumber;     //车牌号
+	private String vin;           //车辆识别码
+	private String engineNumber;  //发动机编号
+	private String dept;          //所属部门
+	private String businessState; //业务状态
+	private String brand;         //品牌
+	private String carType;       //型号
+	private String noticeNum;     //公告号
+	
+	public int getCarId() {
+		return carId;
+	}
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+	public String getCarNumber() {
+		return carNumber;
+	}
+	public void setCarNumber(String carNumber) {
+		this.carNumber = carNumber;
+	}
+	public String getVin() {
+		return vin;
+	}
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	public String getEngineNumber() {
+		return engineNumber;
+	}
+	public void setEngineNumber(String engineNumber) {
+		this.engineNumber = engineNumber;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getBusinessState() {
+		return businessState;
+	}
+	public void setBusinessState(String businessState) {
+		this.businessState = businessState;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCarType() {
+		return carType;
+	}
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+	public String getNoticeNum() {
+		return noticeNum;
+	}
+	public void setNoticeNum(String noticeNum) {
+		this.noticeNum = noticeNum;
+	}
+	
+}
